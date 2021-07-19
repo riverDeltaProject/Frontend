e83e1ff5ac399d0646a629cccbcccfb9562447a6
C12H22O12 <director20844@gmail.com>
2021-07-19 15:41:45 +0900

    21/07/18 item 틀 제작

---
+++ src/components/Item.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="item">
+   <div id="itemImg"></div>
+   <div id="itemInfo">
+     <h3>식당 이름</h3>
+     <p>그러하다</p>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Item',
+  data () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.item{
+  display: flex;
+  flex-wrap: nowrap;
+}
+
+#itemImg{
+  display:inline-block;
+  width:50px;
+  height:50px;
+  background-color: black;
+}
+
+#itemInfo{
+  display:inline-block;
+  margin : 0 0 0 10px;
+}
+
+#itemInfo>*{
+  margin:0;
+}
+
+#itemInfo>p{
+  padding : 4px 0 0 0;
+}
+
+</style>
