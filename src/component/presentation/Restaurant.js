8de3d804e10c63b59d06e9a6c44e921b50468b98
f-lightning <lee6034770@gmail.com>
2021-08-20 23:37:58 +0900

    router

---
+++ src/component/presentation/Restaurant.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Restaurant = () => {
+  return (
+    <div>
+      <h1>Restaurant</h1>
+      <p>At the restaurant</p>
+    </div>
+  );
+};
+
+export default Restaurant;
