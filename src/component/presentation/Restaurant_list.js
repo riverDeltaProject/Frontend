5ff56bb40f04f78f3f4c1bae001fae611f75beed
f-lightning <lee6034770@gmail.com>
2021-08-29 23:49:31 +0900

    식당 결과 검색창 + 모달창 (내부 버튼 없음)

--- src/component/presentation/Restaurant_list.js
+++ src/component/presentation/Restaurant_list.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Link } from "react-router-dom";
+import Modal from './Modal.js';
 
 import backicon from "../assets/backicon.png";
 import scorestar from "../assets/scorestar.png";
+import icon_filter from "../assets/icon_filter.png"
 
 const Restaurant_list = () => {
+
+  const [ modalOpen, setModalOpen ] = useState(false);
+
+    const openModal = () => {
+        setModalOpen(true);
+    }
+    const closeModal = () => {
+        setModalOpen(false);
+    }
 
   return (
     <div>
@@ -16,8 +27,21 @@ const Restaurant_list = () => {
           <div>
             <p className="scoreposition">가게 위치</p>
             <img className="scorestar" src={scorestar} alt="scorestar" />
+            <p className="score">N.N점</p>
           </div>
         </div>
+      </div>
+      <div className="btn_class_list">
+        <form action="search.php" method="post">
+          <input className="btn_text_list" type="text" />
+          <input className="btn_submit_list" type="submit" value="SEARCH" />
+       </form>
+       <React.Fragment>
+            <button className="button_filter" onClick={ openModal }><img className="icon_filter" src={icon_filter} alt="icon_filter" /></button>
+            <Modal open={ modalOpen } close={ closeModal } header="필터">
+              <p>음식종류</p>
+            </Modal>
+        </React.Fragment>
       </div>
     </div>
   );
