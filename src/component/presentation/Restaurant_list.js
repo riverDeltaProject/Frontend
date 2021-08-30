aa2117b72bb3e00b4e57183ca65c87eda262195c
f-lightning <lee6034770@gmail.com>
2021-08-30 23:34:17 +0900

    모달창 내부 (1) : 헤더, 상단 리스트

--- src/component/presentation/Restaurant_list.js
+++ src/component/presentation/Restaurant_list.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import { Link } from "react-router-dom";
-import Modal from './Modal.js';
+import Modal_Restaurant from './Modal_Restaurant.js';
 
 import backicon from "../assets/backicon.png";
 import scorestar from "../assets/scorestar.png";
@@ -38,9 +38,24 @@ const Restaurant_list = () => {
        </form>
        <React.Fragment>
             <button className="button_filter" onClick={ openModal }><img className="icon_filter" src={icon_filter} alt="icon_filter" /></button>
-            <Modal open={ modalOpen } close={ closeModal } header="필터">
-              <p>음식종류</p>
-            </Modal>
+            <Modal_Restaurant open={ modalOpen } close={ closeModal } header="필터">
+              <p className="langoption">음식 종류</p>
+              <div className="foodtype">
+               <button className="itemList2">뷔페</button>
+               <button className="itemList2">아시아</button>
+               <button className="itemList2">양식</button>
+               <button className="itemList2">인도</button>
+               <button className="itemList2">네팔</button>
+               <button className="itemList2">일식</button>
+               <button className="itemList2">중식</button>
+               <button className="itemList2">터키</button>
+               <button className="itemList2">한식</button>
+               <button className="itemList2">중식</button>
+               <button className="itemList2">그 외</button>
+               {/* 3개 이하: 말레이시아, 모로코, 아랍, 이집트, 중동식, 중식, 튀니지, 파키스탄, 프랑스, 우즈베키스탄 분류없음 */}
+              </div>
+              <p className="langoption">Halal Standard</p>
+            </Modal_Restaurant>
         </React.Fragment>
       </div>
     </div>
