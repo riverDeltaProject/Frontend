6e39cc1cad3ffb2da84fadcec88efcb7efefeafc
f-lightning <lee6034770@gmail.com>
2021-09-04 23:45:51 +0900

    카카오맵 (1)

--- src/component/presentation/Restaurant_result.js
+++ src/component/presentation/Restaurant_result.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 import Restaurant_result_axios from './Restaurant_result_axios.js';
 
@@ -6,11 +6,22 @@ import backicon from "../assets/backicon.png";
 
 const Restaurant_result = () => {
 
+  const { kakao } = window;
+  useEffect(() => {
+    const container = document.getElementById('myMap');
+    const options = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    level: 3
+  };
+    const map = new kakao.maps.Map(container, options);
+}, []);
+
   return (
     <div>
       <Link to="./restaurant_list"><img className="backicon" src={backicon} alt="backicon" /></Link>
       <div className="rst_result_prom"/>
       <Restaurant_result_axios/>
+      <div id='myMap'/>
     </div>
   );
 };
