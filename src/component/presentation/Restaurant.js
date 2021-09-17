9686bf5c05baa2e8c82ffa64a5dce177c689939a
C12H22O12 <director20844@gmail.com>
2021-09-17 18:35:52 +0900

    210917 18:35 광역시 버튼 누르면 시/군/구 표시되도록 함 + css 논의

--- src/component/presentation/Restaurant.js
+++ src/component/presentation/Restaurant.js
@@ -1,60 +1,99 @@
-import React,{useState} from 'react';
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {Link} from "react-router-dom";
 import axios from 'axios';
+import SubArea from './SubArea';
 
 import backicon from "../assets/backicon.png";
 
 const Restaurant = () => {
-  const [data, setData] = useState([]);
-  const serviceKey = "GNiCxJ1fY0ZfoZUxoHREWXQNKaQYT9g8t4nGfr%2FvfkA6A5msj3bQfRLVZFgQlhftS1wt9Hh6yJJ%2FH%2FeP%2BxMHpw%3D%3D"; // 서비스키 입력 
+    const [data, setData] = useState();
+    const serviceKey = "GNiCxJ1fY0ZfoZUxoHREWXQNKaQYT9g8t4nGfr%2FvfkA6A5msj3bQfRLVZFgQlhftS1wt9Hh6yJJ%2FH%2FeP%2BxMHpw%3D%3D"; // 서비스키 입력
+    let areaList = [];
 
-  const api = (code) =>{
-    const areaCode = code;
-    const url = `http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=${serviceKey}&areaCode=${areaCode}&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest`;
-   
-    setData([]);
+    const api = (code) => {
+        const areaCode = code;
+        const url = `http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=${serviceKey}&areaCode=${areaCode}&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest`;
 
-    axios.get(url).then((res)=>{
-      console.log("SUCCESS");
-      setData(res.data.response.body.items.item);
+        axios
+            .get(url)
+            .then((res) => {
+                console.log("SUCCESS");
+                const areaCode = res.data.response.body.items.item;
+                // areaCode는 즉각적인 반응이 잘 일어나는 데 비해 useState를 이용한 data는 항상 한발짝 느리다. 왜 그럴까? 이번
+                // 렌더링에서 setData로 값을 설정하고, 그 다음 렌더링에서 해당 값이 반영되기 때문
 
-      console.log(data)
+                setData(areaCode);
+                subArea(areaCode);
+            })
+    }
 
-      // subArea();
-    })
-  }
+    const subArea = (areaCode) => {
+        let length = 10;
 
-  // const subArea = ()=>{
-  //   for (let index = 0; index < data.length; index++) {
-  //     console.log(data);
-  //   }
-  // }
-  
-  return (
-    <div>
-      <Link to="./"><img className="backicon" src={backicon} alt="backicon" /></Link>
-      <h1 className="header2">위치설정</h1>
-      <p className="normalfont">찾고 싶은 위치 및 장소를 입력하세요</p>
-      <div className="btn_class">
-        <form action="search.php" method="post">
-          <input className="btn_text" type="text" />
-          <input className="btn_submit" type="submit" value="SEARCH" />
-       </form>
-      </div>
-      <p className="findingplace">광역시</p>
-      <div className="containerList">
-        <button className="itemList" onClick={()=>{api(1)}}>서울시</button>
-        <button className="itemList" onClick={()=>{api(2)}}>인천시</button>
-        <button className="itemList" onClick={()=>{api(3)}}>대전시</button>
-      </div>
-      <p className="findingplace">시/군/구</p>
-      <div className="containerList2">
-        <button className="itemList2">강남구</button>
-        <button className="itemList2">강동구</button>
-        <button className="itemList2">강서구</button>
-      </div>
-    </div>
-  );
+        for (let key in areaCode){
+          areaList.push(areaCode[key].name)
+        }
+
+        return areaList;
+    }
+
+    return (
+        <div>
+            <Link to="./"><img className="backicon" src={backicon} alt="backicon"/></Link>
+            <h1 className="header2">위치설정</h1>
+            <p className="normalfont">찾고 싶은 위치 및 장소를 입력하세요</p>
+            <div className="btn_class">
+                <form action="search.php" method="post">
+                    <input className="btn_text" type="text"/>
+                    <input className="btn_submit" type="submit" value="SEARCH"/>
+                </form>
+            </div>
+            <p className="findingplace">광역시</p>
+            <div className="containerList">
+                <button
+                    className="itemList"
+                    onClick={() => {
+                        api(1)
+                    }}>서울시</button>
+                <button
+                    className="itemList"
+                    onClick={() => {
+                        api(2)
+                    }}>인천시</button>
+                <button
+                    className="itemList"
+                    onClick={() => {
+                        api(3)
+                    }}>대전시</button>
+                <button
+                    className="itemList"
+                    onClick={() => {
+                        api(4)
+                    }}>대구시</button>
+                <button
+                    className="itemList"
+                    onClick={() => {
+                        api(5)
+                    }}>광주시</button>
+                <button
+                    className="itemList"
+                    onClick={() => {
+                        api(6)
+                    }}>부산시</button>
+                <button
+                    className="itemList"
+                    onClick={() => {
+                        api(39)
+                    }}>제주시</button>
+            </div>
+            <p className="findingplace">시/군/구</p>
+            <div className="containerList2">
+                <SubArea
+                  citylist = {subArea(data)}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Restaurant;
---
+++ src/component/presentation/SubArea.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import {Link} from "react-router-dom";
+import axios from 'axios';
+
+import backicon from "../assets/backicon.png";
+
+const SubArea = ({citylist}) => {
+
+    console.log("SubArea ",citylist);
+
+    const areaBtn = (list) => {
+        let tmp = [];
+    
+        for (let i = 0; i < list.length; i++) {
+            tmp.push(<button className="itemList2" key={citylist[i]}>{citylist[i]}</button>)
+        }
+    
+        console.log(tmp);
+        return tmp;
+    }
+
+    return (
+        <div className="containerList2">
+            {areaBtn(citylist)}
+        </div>
+    );
+};
+
+export default SubArea;
