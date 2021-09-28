85e196f4bdbc3ba4c36bd18285356732a4804c59
C12H22O12 <director20844@gmail.com>
2021-09-28 20:55:41 +0900

    210928 20:55 pagination. 하단 마이너스 표기 오류 수정할 것

---
+++ src/component/presentation/Pagination.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Pagination = ({postsPerPage, start, last, paginate}) => {
+    const pageNumbers = [];
+    for (let i = start; i <= last; i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <ul className="pagination">
+            {
+                pageNumbers.map(number => (
+                    <li key={number}>
+                        <span onClick={() => paginate(number)}>
+                            {number}
+                        </span>
+                    </li>
+                ))
+            }
+        </ul>
+    );
+}
+
+export default Pagination;
